55ce2a5f2a60aa490cdd468e81b6c58b5bf6553a
G'ayrat Sayfullayev <136235060+sayfullayev0204@users.noreply.github.com>
2024-01-25 09:13:37 +0500

    models.py

--- shop/models.py
+++ shop/models.py
@@ -15,13 +15,13 @@ from .models import Category
 from django.urls import reverse
 # Create your models here.
 class Product(models.Model):
-    Turi = models.ForeignKey(Category, on_delete=models.CASCADE)
-    Ismingiz = models.CharField(max_length=100, unique=True)
-    nomi=models.CharField(max_length=500, unique=True)
-    malumoti = models.TextField(max_length=500, blank=True)
-    narxi = models.CharField(max_length=200)
-    rasmi = models.ImageField(upload_to = 'photoes/products')
-    ulanish = models.CharField(max_length=100, unique=True)
+    Turi = models.ForeignKey(Category, on_delete=models.CASCADE,  blank=True, null=True, unique=True)
+    Ismingiz = models.CharField(max_length=100,  blank=True, null=True, unique=True)
+    nomi=models.CharField(max_length=500,  blank=True, null=True, unique=True)
+    malumoti = models.TextField(max_length=500,  blank=True, null=True, unique=True)
+    narxi = models.CharField(max_length=200 , blank=True, null=True, unique=True)
+    rasmi = models.ImageField(upload_to = 'photoes/products', blank=True, null=True, unique=True)
+    ulanish = models.CharField(max_length=100,  blank=True, null=True, unique=True)
     def __str__(self) -> str:
         return self.nomi
 
